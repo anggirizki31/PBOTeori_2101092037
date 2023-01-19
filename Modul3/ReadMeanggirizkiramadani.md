62214c0ee0defd3f8d1d7d8a54ead3c94bd11330
anggirizki31 <114200620+anggirizki31@users.noreply.github.com>
2023-01-19 15:37:47 +0700

    Add files via upload

---
+++ Modul3/ReadMeanggirizkiramadani.md
@@ -0,0 +1,3 @@
+# Modul 3 anggirizkiramadani.java
+#
+Pada program AhmadFarisAlmuzakki.java menggunakan fungsi System.out.println yang digunakan untuk menampilkan teks ke layar monitor. pada program diatas menggunakan fungsi tersebut untuk menampilkan teks "Welcome to Java Programming Ahmad Faris Almuzakki!!!"
---
+++ Modul3/anggirizkiramadani.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Modul3;
+
+/**
+ *
+ * @author asusvivobook
+ */
+public class anggirizkiramadani {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java Programming "+"anggi rizki ramadani");
+    }
+}
